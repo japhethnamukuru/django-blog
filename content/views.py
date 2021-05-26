44b3b4f643ddd654f69039d856a0243e55bc5a55
jeff <japhethnamukuru@gmail.com>
2021-05-26 23:11:21 +0000

    added function based views

--- content/views.py
+++ content/views.py
@@ -5,21 +5,28 @@ from django.views.generic import ListView
 
 from .models import Post
 
-def post_list(request):
-    object_list = Post.published.all()
-    paginator = Paginator(object_list, 3) #3 posts in every page
-    page = request.GET.get('page')
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'content/index.html'
 
-    try:
-        posts = paginator.page(page)
-    except PageNotAnInteger:
-        #if page not an integer deliver the first page 
-        posts = paginator.page(1)
-    except EmptyPage:
-        #if page is out of range deliver last result
-        posts = paginator.page(paginator.num_pages)
+    
+# def post_list(request):
+#     object_list = Post.published.all()
+#     paginator = Paginator(object_list, 3) #3 posts in every page
+#     page = request.GET.get('page')
 
-    return render(request, 'content/index.html', {'page':page, 'posts':posts})
+#     try:
+#         posts = paginator.page(page)
+#     except PageNotAnInteger:
+#         #if page not an integer deliver the first page 
+#         posts = paginator.page(1)
+#     except EmptyPage:
+#         #if page is out of range deliver last result
+#         posts = paginator.page(paginator.num_pages)
+
+#     return render(request, 'content/index.html', {'page':page, 'posts':posts})
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post, status='published', publish__year=year,
